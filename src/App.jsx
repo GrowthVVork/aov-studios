f7d0ce238ba9911c4b8166894714f2d1b227f0ca
siddhi-gh <siddhiusgupta@gmail.com>
2023-06-25 16:59:35 +0530

    FIX: fix App file extension

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,12 @@ const App = () => {
         <Blog />
       </main>
       <Footer />
+      <div className="overlay">
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+        <div className="col"></div>
+      </div>
     </>
   );
 };
