523efe1e948d012efb03afad691e606e5478fcae
siddhi-gh <siddhiusgupta@gmail.com>
2023-06-26 22:10:31 +0530

    CHORE: prettify Header code

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -14,58 +14,58 @@ export const Header = () => {
 
   return (
     <div className="headerDiv">
-    <header className={`contained ${navActive ? "active" : ""}`}>
-      <h1 className="visually-hidden">
-        An architecture and interior design firm
-      </h1>
-      <a href="/" className="logo">
-        AOV Studios
-      </a>
+      <header className={`contained ${navActive ? "active" : ""}`}>
+        <h1 className="visually-hidden">
+          An architecture and interior design firm
+        </h1>
+        <a href="/" className="logo">
+          AOV Studios
+        </a>
 
-      <span
-        className={`menu-icon hamburger-menu ${navActive ? "active" : ""}`}
-        onClick={toggleResponsiveNav}
-      >
-        &#9776;
-      </span>
-      <nav className={`nav ${navActive ? "active" : ""}`}>
         <span
-          className={`menu-icon close-menu ${
-            navActive ? "contained active" : ""
-          }`}
+          className={`menu-icon hamburger-menu ${navActive ? "active" : ""}`}
           onClick={toggleResponsiveNav}
         >
-          &#215;
+          &#9776;
         </span>
-        <ul className={`nav-list ${navActive ? "contained" : ""}`}>
-          <li>
-            <a href="#hero" onClick={handleNavLinkClick}>
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="#about" onClick={handleNavLinkClick}>
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#services" onClick={handleNavLinkClick}>
-              Services
-            </a>
-          </li>
-          <li>
-            <a href="#portfolio" onClick={handleNavLinkClick}>
-              Portfolio
-            </a>
-          </li>
-          <li>
-            <a href="#footer" onClick={handleNavLinkClick}>
-              Contact
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={`nav ${navActive ? "active" : ""}`}>
+          <span
+            className={`menu-icon close-menu ${
+              navActive ? "contained active" : ""
+            }`}
+            onClick={toggleResponsiveNav}
+          >
+            &#215;
+          </span>
+          <ul className={`nav-list ${navActive ? "contained" : ""}`}>
+            <li>
+              <a href="#hero" onClick={handleNavLinkClick}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#about" onClick={handleNavLinkClick}>
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#services" onClick={handleNavLinkClick}>
+                Services
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" onClick={handleNavLinkClick}>
+                Portfolio
+              </a>
+            </li>
+            <li>
+              <a href="#footer" onClick={handleNavLinkClick}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 };
