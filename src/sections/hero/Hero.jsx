8788178b968166a11c965a114bd1623881a5d1d7
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-16 22:12:27 +0530

    FIX: fix Hero picture

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import hero from "../../assets/hero/hero.jpg";
+import hero from "../../assets/hero/hero.png";
 
 export const Hero = () => {
   return (
-    <picture class="contained" id="hero">
+    <picture class="contained" id="hero" style={{ marginTop: "6.5rem" }}>
+      {/* <source srcset={hero} media="(min-width: 850px)" /> */}
       <img srcset={hero} alt="" />
     </picture>
   );
