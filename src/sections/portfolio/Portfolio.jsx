ce2bf9bcae28eb7218aba973e702e871d3f38652
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-16 17:44:41 +0530

    CHORE: rename portfolio section assets

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import one from "../../assets/gallery/1.jpg";
-import two from "../../assets/gallery/2.jpg";
-import three from "../../assets/gallery/3.jpg";
-import four from "../../assets/gallery/4.jpg";
-import five from "../../assets/gallery/5.jpg";
-import six from "../../assets/gallery/6.jpg";
-import seven from "../../assets/gallery/7.jpg";
+import one from "../../assets/portfolio/1.jpg";
+import two from "../../assets/portfolio/2.jpg";
+import three from "../../assets/portfolio/3.jpg";
+import four from "../../assets/portfolio/4.jpg";
+import five from "../../assets/portfolio/5.jpg";
+import six from "../../assets/portfolio/6.jpg";
+import seven from "../../assets/portfolio/7.jpg";
 import "./portfolioStyles.css";
 
 export const Portfolio = () => {
