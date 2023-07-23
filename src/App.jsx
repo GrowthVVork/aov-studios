1d9cef36874e92e85dd06d37a2ee37a457606252
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-23 19:25:06 +0530

    FIX: fix App

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
 import React from "react";
-import { Header, Hero, About, Services, Portfolio, Footer } from "./sections";
+// import { Header, Hero, About, Services, Portfolio, Footer } from "./sections";
+import { AboutUs, Header, HeroImage } from "./components";
 
 const App = () => {
+  // return (
+  //   <>
+  //     <Header />
+  //     <Hero />
+  //     <main>
+  //       <About />
+  //       <Services />
+  //       <Portfolio />
+  //       {/* Blog to be replaced by projects which will be another page */}
+  //       {/* <Blog /> */}
+  //     </main>
+  //     <Footer />
+  //     <div className="overlay">
+  //       <div className="col"></div>
+  //       <div className="col"></div>
+  //       <div className="col"></div>
+  //       <div className="col"></div>
+  //     </div>
+  //   </>
+  // );
   return (
     <>
       <Header />
-      <Hero />
-      <main>
-        <About />
-        <Services />
-        <Portfolio />
-        {/* Blog to be replaced by projects which will be another page */}
-        {/* <Blog /> */}
-      </main>
-      <Footer />
-      <div className="overlay">
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-        <div className="col"></div>
-      </div>
+      <HeroImage />
+      <AboutUs/>
     </>
   );
 };
