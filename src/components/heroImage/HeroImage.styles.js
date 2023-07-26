f7f8738ebf343f5d3aa9875153efaa9cf8473b4c
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-26 21:46:29 +0530

    REFACTOR: fix Hero styles

--- src/components/heroImage/HeroImage.styles.js
+++ src/components/heroImage/HeroImage.styles.js
@@ -1,13 +1,14 @@
 import { styled } from "@mui/material";
 
 export const HeroImageWrapper = styled("picture")(() => ({
-  display: "flex",
-  justifyContent: "center",
-  paddingTop: "8rem",
-  height: "100%",
-  width: "100%",
+  maxWidth: "90rem",
+  width: "90%",
 }));
 
 export const Image = styled("img")(() => ({
+  display: "block",
+  margin: "0 auto",
+  maxWidth: "100%",
+  paddingTop: "8rem",
   width: "90%",
 }));
