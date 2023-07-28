0b72f015d1f16da27bf0fcb3b1da83f68acce98b
siddhi-gh <siddhiusgupta@gmail.com>
2023-07-28 17:03:24 +0530

    CHORE: code cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Header, Hero, About, Services, Portfolio, Footer } from "./sections";
 import {
   AboutUs,
   Footer,
@@ -10,26 +9,6 @@ import {
 } from "./components";
 
 const App = () => {
-  // return (
-  //   <>
-  //     <Header />
-  //     <Hero />
-  //     <main>
-  //       <About />
-  //       <Services />
-  //       <Portfolio />
-  //       {/* Blog to be replaced by projects which will be another page */}
-  //       {/* <Blog /> */}
-  //     </main>
-  //     <Footer />
-  //     <div className="overlay">
-  //       <div className="col"></div>
-  //       <div className="col"></div>
-  //       <div className="col"></div>
-  //       <div className="col"></div>
-  //     </div>
-  //   </>
-  // );
   return (
     <>
       <Header />
