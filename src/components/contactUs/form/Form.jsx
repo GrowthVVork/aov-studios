32840752ee634eff9be62afbe662ec1eefbc30d5
siddhi-gh <siddhiusgupta@gmail.com>
2023-08-06 13:19:30 +0530

    FEAT: add Form component

---
+++ src/components/contactUs/form/Form.jsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { CommonHeading, CommonWrapper } from "../../Shared.styles";
+
+export const Form = () => {
+  return (
+    <CommonWrapper>
+      <CommonHeading>Form</CommonHeading>
+    </CommonWrapper>
+  );
+};
