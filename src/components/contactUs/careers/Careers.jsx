5543d3b151b54d38174b7d19f45620e233502690
siddhi-gh <siddhiusgupta@gmail.com>
2023-08-06 13:20:21 +0530

    FIX: use common styles

--- src/components/contactUs/careers/Careers.jsx
+++ src/components/contactUs/careers/Careers.jsx
@@ -1,20 +1,18 @@
 import * as React from "react";
-import { CareersContent, CareersWrapper } from "./Careers.styles";
+import { CommonHeading, CommonText, CommonWrapper } from "../../Shared.styles";
 
 export const Careers = () => {
   return (
-    <CareersWrapper>
-      <CareersContent
-        style={{ color: "#767676", fontSize: "1.8rem", fontWeight: "bold" }}
-      >
-        Careers
-      </CareersContent>
-      <CareersContent>WE ARE HIRIING!!</CareersContent>
-      <CareersContent>
+    <CommonWrapper>
+      <CommonHeading>Careers</CommonHeading>
+      <CommonText style={{ paddingBottom: "1rem" }}>
+        WE ARE HIRIING!!
+      </CommonText>
+      <CommonText>
         We are looking for fresh graduates as well as experienced professionals.
         Interested candidates should submit a cover letter and resume at
         studiosaov@gmail.com
-      </CareersContent>
-    </CareersWrapper>
+      </CommonText>
+    </CommonWrapper>
   );
 };
