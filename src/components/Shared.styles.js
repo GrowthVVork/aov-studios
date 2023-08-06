f731cca00e1b13f47f4e8d4197f1037d3a3aaa73
siddhi-gh <siddhiusgupta@gmail.com>
2023-08-06 13:19:58 +0530

    FEAT: move common styles to shared

--- src/components/Shared.styles.js
+++ src/components/Shared.styles.js
@@ -1,4 +1,4 @@
-import { styled } from "@mui/material";
+import { styled, Typography } from "@mui/material";
 
 export const SectionContainer = styled("div")(() => ({
   margin: "0 auto",
@@ -16,4 +16,30 @@ export const SectionHeading = styled("h4")(() => ({
   marginBottom: "3rem",
   textAlign: "center",
   textTransform: "uppercase",
+}));
+
+export const CommonWrapper = styled("div")(() => ({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  width: "100%",
+}));
+
+export const CommonHeading = styled(Typography)(() => ({
+  color: "#767676",
+  font: '300 1rem/1.7 "Poppins", sans-serif',
+  fontSize: "1.8rem",
+  fontWeight: "bold",
+  paddingBottom: "1rem",
+  "@media (max-width: 53rem)": {
+    textAlign: "center",
+  },
+}));
+
+export const CommonText = styled(Typography)(() => ({
+  font: '300 1rem/1.7 "Poppins", sans-serif',
+  fontSize: "1.6rem",
+  "@media (max-width: 53rem)": {
+    textAlign: "center",
+  },
 }));
