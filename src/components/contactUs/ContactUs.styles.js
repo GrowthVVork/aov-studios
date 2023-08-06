c2a91d758a59869e8ddd6abfdbc9e06a8de2f3fe
siddhi-gh <siddhiusgupta@gmail.com>
2023-08-07 00:13:05 +0530

    CHORE: prettify code

--- src/components/contactUs/ContactUs.styles.js
+++ src/components/contactUs/ContactUs.styles.js
@@ -3,7 +3,8 @@ import { styled } from "@mui/material";
 export const ContactWrapper = styled("div")(() => ({
   display: "flex",
   gap: "8rem",
-  "@media (max-width: 40rem)": { // to be updated to "53rem" if Careers section is integrated.
+  "@media (max-width: 40rem)": {
+    // to be updated to "53rem" if Careers section is integrated.
     flexDirection: "column",
     gap: "3rem",
     padding: "0 2rem",
