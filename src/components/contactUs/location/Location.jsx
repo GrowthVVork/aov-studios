6cbe7acc0cf3ecd20660b5a75ea4213ae842aa0f
siddhi-gh <siddhiusgupta@gmail.com>
2023-08-06 13:20:38 +0530

    FEAT: integrate google maps

--- src/components/contactUs/location/Location.jsx
+++ src/components/contactUs/location/Location.jsx
@@ -1,5 +1,46 @@
 import * as React from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import { CircularProgress } from "@mui/material";
+import { CommonHeading, CommonText, CommonWrapper } from "../../Shared.styles";
+import { Email, MapWrapper } from "./Location.styles";
 
 export const Location = () => {
-  return;
+  const containerStyle = {
+    width: 400,
+    height: 300,
+  };
+
+  const center = {
+    lat: 22.73223246787647,
+    lng: 75.85844404914533,
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  return (
+    <CommonWrapper>
+      <CommonHeading>Our Office</CommonHeading>
+      <MapWrapper>
+        {isLoaded ? (
+          <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+          >
+            <Marker position={center} />
+          </GoogleMap>
+        ) : (
+          <CircularProgress />
+        )}
+      </MapWrapper>
+      <CommonText style={{ fontWeight: "bold" }}>AOV Studios</CommonText>
+      <CommonText>Narayan Bagh, Indore (452007)</CommonText>
+      <CommonText style={{ paddingBottom: "1rem" }}>MP, IN</CommonText>
+      <CommonText>+91-7582898292</CommonText>
+      <Email href="mailto:studiosaov@gmail.com">studiosaov@gmail.com</Email>
+    </CommonWrapper>
+  );
 };
